5896d27d83b4aeab27a2422ce366ff91d0f42357
John Devlin <johnp.devlin2@gmail.com>
2024-01-29 15:41:49 +0000

    add hours on in label, align more with styling and override buggy error

--- src/components/Input/DateTimeSelect.tsx
+++ src/components/Input/DateTimeSelect.tsx
@@ -24,10 +24,10 @@ const DateTimeSelect = ({
 	return (
 		<LocalizationProvider dateAdapter={AdapterDayjs}>
 			<DateTimePicker
-				format='DD/MM/YYYY'
+				format='DD/MM/YYYY hh:mm'
 				label={label}
-				disablePast={onlyAllowFuture}
 				openTo='hours'
+				disablePast={onlyAllowFuture}
 				value={dayjs(value)}
 				aria-label={`Input Date and Time for ${label}`}
 				onChange={(newValue) => {
@@ -36,26 +36,75 @@ const DateTimeSelect = ({
 					}
 				}}
 				data-test-id={dataTestId}
-				sx={{
-					'& label.Mui-focused': {
-						color: 'primary.main',
-					},
-					'& .MuiInputLabel-root': {
-						color: 'ButtonShadow',
-					},
-
-					'& .MuiOutlinedInput-root.Mui-error .MuiOutlinedInput-notchedOutline':
-						{
-							borderColor: 'primary.main',
-
-							'&:hover': {
-								backgroundColor: 'grey',
-							},
-						},
-				}}
+				sx={styleProps}
 			/>
 		</LocalizationProvider>
 	);
 };
 
 export default DateTimeSelect;
+
+const styleProps = {
+	color: 'rgba(0, 0, 0, 0.60)',
+	backgroundColor: 'rgba(0, 0, 0, 0.06)',
+	borderTopLeftRadius: '4px',
+	borderTopRightRadius: '4px',
+	// Override error class here as component mistakenly throws errors and changes colour
+	'& .MuiInputLabel-root.Mui-error': {
+		color: 'rgba(0, 0, 0, 0.60)',
+	},
+
+	'& .MuiOutlinedInput-root': {
+		position: 'relative',
+		'& fieldset': {
+			border: 'none',
+		},
+		'&::before': {
+			content: '""',
+			position: 'absolute',
+			left: 0,
+			right: 0,
+			bottom: 0,
+			borderBottom: '1px solid rgba(0, 0, 0, 0.42)',
+		},
+		'&:after': {
+			borderBottom: '1px solid rgba(0, 0, 0, 0.42)',
+			transform: 'scaleX(0)',
+			transformOrigin: 'center',
+			transition: 'transform 200ms cubic-bezier(0.0, 0, 0.2, 1)',
+		},
+		'&:hover::before, &:hover::after': {
+			borderBottom: '1px solid rgba(0, 0, 0, 0.87)',
+			color: 'rgba(0, 0, 0, 0.87)',
+		},
+
+		'&.Mui-focused::before': {
+			transform: 'scaleX(1)',
+			transformOrigin: 'center',
+			transition: 'transform 200ms cubic-bezier(0.0, 0, 0.2, 1)',
+			borderBottom: '2px solid',
+			borderColor: 'primary.main',
+		},
+		' &.Mui-focused::after': {
+			transform: 'scaleX(0)',
+			transformOrigin: 'center',
+			transition: 'transform 200ms cubic-bezier(0.0, 0, 0.2, 1)',
+			borderBottom: '2px solid',
+			borderColor: 'primary.main',
+		},
+	},
+	'& .MuiInputLabel-root': {
+		color: 'rgba(0, 0, 0, 0.60)',
+		'&.Mui-focused': {
+			color: 'primary.main',
+		},
+
+		'& .MuiInputBase-root': {
+			paddingRight: '20px',
+			transition: 'background-color 200ms cubic-bezier(0.0, 0, 0.2, 1)',
+			'&:hover': {
+				backgroundColor: 'rgba(0, 0, 0, 0.04)',
+			},
+		},
+	},
+};
